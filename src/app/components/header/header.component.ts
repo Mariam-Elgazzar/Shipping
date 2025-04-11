f458539d8073c1aa90ef49ab7304b357b07e5f46
AminaElagmy <aminaelagmy@gmail.com>
2025-04-11 23:39:40 +0200

    header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,23 +1,38 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from '../../services/auth.service';
-import { MatIcon } from '@angular/material/icon';
-import { MatNavList } from '@angular/material/list';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatToolbar } from '@angular/material/toolbar';
-import { MatMenuModule } from '@angular/material/menu';
+import { Component } from "@angular/core"
+import { Router } from "@angular/router"
+import { AuthService } from "../../services/auth.service"
+import { MatIcon } from "@angular/material/icon"
+import { MatNavList } from "@angular/material/list"
+import { MatDialogModule } from "@angular/material/dialog"
+import { MatToolbar } from "@angular/material/toolbar"
+import { MatMenuModule } from "@angular/material/menu"
+import { CommonModule } from "@angular/common"
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
-  imports: [MatIcon, MatNavList, MatDialogModule, MatToolbar, MatMenuModule],
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
+  imports: [CommonModule, MatIcon, MatNavList, MatDialogModule, MatToolbar, MatMenuModule],
+  standalone: true,
 })
 export class HeaderComponent {
-  constructor(private router: Router, public authService: AuthService) {}
+  isMobileMenuOpen = false
+
+  constructor(
+    private router: Router,
+    public authService: AuthService,
+  ) {}
+
+  toggleMobileMenu(): void {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen
+  }
+
+  closeMobileMenu(): void {
+    this.isMobileMenuOpen = false
+  }
 
   logout(): void {
-    this.authService.logout();
-    this.router.navigate(['/login']);
+    this.authService.logout()
+    this.router.navigate(["/login"])
   }
 }
