591c0f92828f68f4c56fd5a7af82a4a5e6e2d2d1
nancy soliman <nancysoliman82@gmail.com>
2025-04-12 12:25:50 +0200

    edit order table

--- src/app/components/order/order-dashboard.component.ts
+++ src/app/components/order/order-dashboard.component.ts
@@ -1,15 +1,17 @@
-import { Component, type OnInit } from "@angular/core"
+import { Component,  OnInit } from "@angular/core"
 import { CommonModule } from "@angular/common"
 import { HeaderComponent } from "../header/header.component"
-import { OrderStatsComponent } from "./order-stats/order-stats.component"
+import { OrderBannerComponent } from "./order-banner/order-banner.component"
 import { OrderTableComponent } from "./order-table/order-table.component"
+import { ItemsPackageComponent } from "./items-package/items-package.component"
 
 @Component({
   selector: "app-order-dashboard",
   standalone: true,
-  imports: [CommonModule, OrderStatsComponent, OrderTableComponent],
-  templateUrl:"./order-dashboard.components.html",
-  styleUrls: ["./order-dashboard.components.scss"],
+  imports: [CommonModule, OrderBannerComponent, OrderTableComponent, ItemsPackageComponent],
+  templateUrl:"order-dashboard.components.html",
+  styleUrls: ["order-dashboard.components.scss"],
+
 })
 export class OrderDashboardComponent implements OnInit {
   constructor() {}
