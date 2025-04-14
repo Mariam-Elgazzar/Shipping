61eae17ba5ba2a08dfaa0bd285e332fafef01f8a
ahmeddawod10 <dawodahmed10@gmail.com>
2025-04-14 15:12:55 +0200

    edit-update

---
+++ src/app/components/branches/branche-add/branche-add.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-branche-add',
+  imports: [],
+  templateUrl: './branche-add.component.html',
+  styleUrl: './branche-add.component.scss'
+})
+export class BrancheAddComponent {
+
+}
